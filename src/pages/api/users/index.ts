ddf137dcd5cd8812477ce691dd9bc146ec68c22b
root <henrique_hgm1996@hotmail.com>
2024-07-06 15:27:34 -0300

    test3 renderizar a lista de usuarios

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -14,8 +14,23 @@ import { NextApiRequest, NextApiResponse } from 'next/types';
 
 import { IUser } from '@/types/user.d';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-	const users: Array<unknown> = [];
+const users: IUser[] = [
+    { id: 1, name: 'João Silva', email: 'joao.silva@example.com' },
+    { id: 2, name: 'Maria Oliveira', email: 'maria.oliveira@example.com' },
+    { id: 3, name: 'Carlos Souza', email: 'carlos.souza@example.com' },
+    { id: 4, name: 'Ana Pereira', email: 'ana.pereira@example.com' },
+    { id: 5, name: 'Paulo Lima', email: 'paulo.lima@example.com' },
+    { id: 6, name: 'Juliana Santos', email: 'juliana.santos@example.com' },
+    { id: 7, name: 'Bruno Costa', email: 'bruno.costa@example.com' },
+    { id: 8, name: 'Fernanda Almeida', email: 'fernanda.almeida@example.com' },
+    { id: 9, name: 'Rodrigo Martins', email: 'rodrigo.martins@example.com' },
+    { id: 10, name: 'Patrícia Ferreira', email: 'patricia.ferreira@example.com' }
+  ];
 
-	return res.status(500).json(users);
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'GET') {
+        return res.status(200).json(users);
+      } else {
+        return res.status(405).json({ message: 'Method Not Allowed' });
+    }
 };
