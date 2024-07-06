5a9b04e123dd5eac82f166be653e7e4bc16f0085
root <henrique_hgm1996@hotmail.com>
2024-07-06 20:02:42 -0300

    test4 - implementa funcionalidades do forms

--- src/pages/formulario.tsx
+++ src/pages/formulario.tsx
@@ -10,21 +10,53 @@
  */
 
 import styles from '@/styles/formulario.module.css';
+import { useForm } from 'react-hook-form';
 
 export default function Form() {
-	function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-		event.preventDefault();
+    type Data = {
+        name: string;
+        email: string;
+    }
 
-		console.log('submit');
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Data>();
+
+	async function submitForm(data: Data) {
+        try{
+            const response = await fetch('/api/users/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to create user');
+            }
+
+            const result = await response.json();
+            console.log('User created:', result);
+
+
+            console.log('response', response);
+
+        } catch (error: any) {
+            console.error('Error', error.message)
+        }
 	}
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.content}>
-				<form onSubmit={handleSubmit}>
-					<input type="text" placeholder="Name" />
-					<input type="email" placeholder="E-mail" />
-
+				<form onSubmit={handleSubmit((data) => submitForm(data))}>
+					<input type="text" placeholder="Name" {...register('name', {required: true })} />
+                    {errors.name && <p>Name is required.</p>}
+					<input type="email" placeholder="E-mail" {...register('email', {required: true })} />
+                    {errors.email && <p>E-mail is required.</p>}
 					<button type="submit" data-type="confirm">
 						Enviar
 					</button>
